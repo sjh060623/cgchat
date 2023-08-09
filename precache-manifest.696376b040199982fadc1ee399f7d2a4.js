59df8616f4f4a1cb4f7b97d4714b6c45dfc86bd1
SonJeongHoon <77915345+sjh060623@users.noreply.github.com>
2023-08-09 21:01:25 +0900

    Add files via upload

---
+++ precache-manifest.696376b040199982fadc1ee399f7d2a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7ba502c88f82fbdfffce2dc41ea2c1",
+    "url": "/cgchat/index.html"
+  },
+  {
+    "revision": "8507c7654843fcdcc923",
+    "url": "/cgchat/static/css/main.1ff5616d.chunk.css"
+  },
+  {
+    "revision": "d7c6e9a54161d34a3116",
+    "url": "/cgchat/static/js/2.6d04462b.chunk.js"
+  },
+  {
+    "revision": "34981cc0942697370fc9d635062a9d94",
+    "url": "/cgchat/static/js/2.6d04462b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8507c7654843fcdcc923",
+    "url": "/cgchat/static/js/main.6d6a43ec.chunk.js"
+  },
+  {
+    "revision": "21df894aeecc39260a0d",
+    "url": "/cgchat/static/js/runtime-main.6a1cbd21.js"
+  },
+  {
+    "revision": "36832d8a62e404d88a6e27a464b509a6",
+    "url": "/cgchat/static/media/font.36832d8a.otf"
+  },
+  {
+    "revision": "213e7b53228f27745da6a96463c3b5de",
+    "url": "/cgchat/static/media/logoa.213e7b53.png"
+  },
+  {
+    "revision": "61de5ee99be5d0624ef6038e04b34503",
+    "url": "/cgchat/static/media/logob.61de5ee9.png"
+  }
+]);
